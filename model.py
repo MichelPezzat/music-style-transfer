8be900ec31d376baa5116bfe700f9210582603ea
Michel <mipemol@gmail.com>
2020-01-22 23:39:31 -0600

    still debugging

--- model.py
+++ model.py
@@ -178,7 +178,7 @@ class StarGAN(object):
             [self.generated_forward, self.generated_back, self.generator_loss, self.generator_optimizer, self.generator_summaries], \
             feed_dict = {self.lambda_cycle: lambda_cycle, self.lambda_identity: lambda_identity, self.lambda_classifier:lambda_classifier ,\
             self.input_real: input_source, self.target_real: input_target,\
-             self.source_label:source_label, self.input_mixed: input_mixed, self.mixed_label: mixed_label , self.target_label:target_label, \
+             self.source_label:source_label, self.target_label:target_label, \
              self.generator_learning_rate: generator_learning_rate,self.gaussian_noise: gaussian_noise})
 
         self.writer.add_summary(generator_summaries, self.train_step)
