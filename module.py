48eb2b5748a33a178f71c28a36887a8a62f38b77
Michel <mipemol@gmail.com>
2020-01-24 13:51:51 -0600

    fixing

--- module.py
+++ module.py
@@ -688,7 +688,7 @@ def PhraseDiscriminator(in_tensor, reuse=False, name='discriminator'):
     return h3
 
 def discriminator_c(x_init, reuse=False, name="discriminator"):
-        with tf.variable_scope(scope, reuse=reuse) :
+        with tf.variable_scope(name, reuse=reuse) :
             channel = self.ch
             x = conv(x_init, channel, kernel=4, stride=2, pad=1, use_bias=True, scope='conv_0')
             x = lrelu(x, 0.01)
